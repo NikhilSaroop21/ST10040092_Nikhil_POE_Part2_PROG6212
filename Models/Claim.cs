c4a0d2c7b478245dbe3b5acadf00cf3474cf652c
NikhilSaroop21 <128732882+NikhilSaroop21@users.noreply.github.com>
2024-11-22 10:40:02 +0200

    Update Claim.cs

--- Models/Claim.cs
+++ Models/Claim.cs
@@ -55,3 +55,11 @@ namespace ST10040092_Nikhil_POE_Part2_PROG6212.Models
         public string UserId { get; set; }
     }
 }
+
+// code attribution // W3schools // https://www.w3schools.com/cs/index.php
+
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/getting-started
+
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/azure/storage/blobs/storage-blob-dotnet-get-started?tabs=azure-ad
+
+// code attribution //Bootswatch //https://bootswatch.com/
