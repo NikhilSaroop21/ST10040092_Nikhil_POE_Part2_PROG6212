22d1838808df19586a9743579f74ec1e64aae96d
NikhilSaroop21 <128732882+NikhilSaroop21@users.noreply.github.com>
2024-11-22 10:40:13 +0200

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -7,3 +7,10 @@ namespace ST10040092_Nikhil_POE_Part2_PROG6212.Models
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
+// code attribution // W3schools // https://www.w3schools.com/cs/index.php
+
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/getting-started
+
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/azure/storage/blobs/storage-blob-dotnet-get-started?tabs=azure-ad
+
+// code attribution //Bootswatch //https://bootswatch.com/
