b4c2e4b6ce3b1d07296ef8580816e0d4b1896796
NikhilSaroop21 <128732882+NikhilSaroop21@users.noreply.github.com>
2024-11-22 10:39:41 +0200

    Update HomeController.cs
    Code attribution is critical in software development as it ensures credit is given to the original authors of code or libraries, fostering a culture of respect and transparency within the developer community. Proper attribution not only recognizes the hard work and intellectual property of creators but also promotes trust and ethical collaboration. Additionally, it provides clarity about the origins of external code, helping developers and organizations maintain compliance with licensing requirements, avoid legal issues, and support open-source projects. By clearly attributing code, teams can also improve traceability, making it easier to address bugs or implement future updates.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -30,3 +30,10 @@ namespace ST10040092_Nikhil_POE_Part2_PROG6212.Controllers
         }
     }
 }
+// code attribution // W3schools // https://www.w3schools.com/cs/index.php
+
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/getting-started
+
+// code attribution //Microsoft //https://learn.microsoft.com/en-us/azure/storage/blobs/storage-blob-dotnet-get-started?tabs=azure-ad
+
+// code attribution //Bootswatch //https://bootswatch.com/
